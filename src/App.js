5cb13cff25eaaafdf9d343d22ce610f61f50eb32
unknown <miguel.semedo.2@outlook.com>
2022-10-11 11:04:26 +0100

    Add Sass

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+import './categories.styles.scss';
+
 const App = () => {
 	const categories = [
 		{
 			id: 1,
 			title: 'Hats',
 		},
+		{
+			id: 1,
+			title: 'Jackets',
+		},
+		{
+			id: 1,
+			title: 'Sneakers',
+		},
+		{
+			id: 1,
+			title: 'Women',
+		},
+		{
+			id: 1,
+			title: 'Men',
+		},
 	];
 
 	return (
 		<div className="categories-container">
-			{categories.map(({ title }) => (
-				<div className="category-container">
+			{categories.map(({ title, id }) => (
+				<div key={id} className="category-container">
 					<div className="background-image" />
 					<div className="category-body-container">
 						<h2>{title}</h2>
