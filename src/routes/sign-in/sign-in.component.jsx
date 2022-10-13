66f887c2ece3064f016274472cae076158314e0b
unknown <miguel.semedo.2@outlook.com>
2022-10-13 10:04:51 +0100

    Firebase Authenticate

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,18 @@
+import {
+	signInWithGooglePopup,
+	createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.utils';
+
 const SignIn = () => {
+	const logGoogleUser = async () => {
+		const { user } = await signInWithGooglePopup();
+		createUserDocumentFromAuth(user);
+	};
+
 	return (
 		<div>
 			<h1>Sign In Page</h1>
+			<button onClick={logGoogleUser}>Sign in with google Popup</button>
 		</div>
 	);
 };
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,38 @@
+import { initializeApp } from 'firebase/app';
+import {
+	getAuth,
+	signInWithRedirect,
+	signInWithPopup,
+	GoogleAuthProvider,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyDC1bHOKpSylUZHa3Y849-mz16J8dR_YeU',
+	authDomain: 'crwn-clothing-db-47571.firebaseapp.com',
+	projectId: 'crwn-clothing-db-47571',
+	storageBucket: 'crwn-clothing-db-47571.appspot.com',
+	messagingSenderId: '84349685631',
+	appId: '1:84349685631:web:61a5a69a00aa6818fad44a',
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+	prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+	const userDocRef = doc(db, 'users', userAuth.uid);
+
+	console.log(userDocRef);
+
+	const userSnapshot = await getDoc(userDocRef);
+	console.log(userSnapshot.exists());
+};
