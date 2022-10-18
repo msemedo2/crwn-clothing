bcfb489ef7e888350292f3236a0ee563f9e97c40
unknown <miguel.semedo.2@outlook.com>
2022-10-18 10:12:39 +0100

    Shop Page Set Up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import NavigationBar from './routes/navigation/navigation-bar.component';
 import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
 
 import Home from './routes/home/home.component';
 
@@ -9,7 +10,7 @@ const App = () => {
 		<Routes>
 			<Route path="/" element={<NavigationBar />}>
 				<Route index element={<Home />} />
-				{/* <Route index element={<Shop />} /> */}
+				<Route path="shop" element={<Shop />} />
 				<Route path="auth" element={<Authentication />} />
 			</Route>
 		</Routes>
