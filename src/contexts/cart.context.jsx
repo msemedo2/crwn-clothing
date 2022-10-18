a1f81af137c988e8f66ce33721dd578fe5fc9ee3
unknown <miguel.semedo.2@outlook.com>
2022-10-18 19:21:28 +0100

    Cart Total

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -49,12 +49,14 @@ export const CartContext = createContext({
 	removeItemFromCart: () => {},
 	cartCount: 0,
 	clearItem: () => {},
+	cartTotal: 0,
 });
 
 export const CartProvider = ({ children }) => {
 	const [isCartOpen, setIsCartOpen] = useState(false);
 	const [cartItems, setCartItems] = useState([]);
 	const [cartCount, setCartCount] = useState(0);
+	const [cartTotal, setCartTotal] = useState(0);
 
 	useEffect(() => {
 		const newCartCount = cartItems.reduce(
@@ -62,6 +64,14 @@ export const CartProvider = ({ children }) => {
 			0
 		);
 		setCartCount(newCartCount);
+	}, [cartItems]);
+
+	useEffect(() => {
+		const newCartTotal = cartItems.reduce(
+			(total, cartItem) => total + cartItem.quantity * cartItem.price,
+			0
+		);
+		setCartTotal(newCartTotal);
 	}, [cartItems]);
 
 	const addItemToCart = (productToAdd) => {
@@ -84,6 +94,7 @@ export const CartProvider = ({ children }) => {
 		clearItemFromCart,
 		cartItems,
 		cartCount,
+		cartTotal,
 	};
 
 	return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
