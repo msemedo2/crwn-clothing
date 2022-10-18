f0b995b753544838459300d48bebee2f06fe3349
unknown <miguel.semedo.2@outlook.com>
2022-10-18 10:31:39 +0100

    Products Context

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<UserProvider>
-				<App />
+				<ProductsProvider>
+					<App />
+				</ProductsProvider>
 			</UserProvider>
 		</BrowserRouter>
 	</React.StrictMode>
