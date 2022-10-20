e843ffd0d23c0cded2202a337a10cddcd60f4fb4
unknown <miguel.semedo.2@outlook.com>
2022-10-20 15:23:10 +0100

    Category Page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,13 +1,12 @@
 import { Routes, Route } from 'react-router';
 import CategoriesPreview from '../categories-preview/category-preview.component';
+import Category from '../category/category.component';
 
 const Shop = () => {
 	return (
 		<Routes>
 			<Route index element={<CategoriesPreview />} />
-			{/* <Route path='/hats'></Route>
-				<Route path=''></Route>
-				<Route path=''></Route> */}
+			<Route path=":category" element={<Category />} />
 		</Routes>
 	);
 };
