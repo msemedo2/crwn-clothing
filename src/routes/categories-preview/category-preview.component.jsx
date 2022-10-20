01af8e18f5f6afa00fed739a37505aa1df9b7e65
unknown <miguel.semedo.2@outlook.com>
2022-10-20 17:15:55 +0100

    Fix Clashing Styles

--- src/routes/categories-preview/category-preview.component.jsx
+++ src/routes/categories-preview/category-preview.component.jsx
@@ -1,4 +1,5 @@
 import { useContext, Fragment } from 'react';
+
 import CategoryPreview from '../../components/category-preview/category-preview.component';
 
 import { CategoriesContext } from '../../contexts/categories.context';
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from 'react';
+import { useContext, useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { CategoriesContext } from '../../contexts/categories.context';
@@ -16,12 +16,15 @@ const Category = () => {
 	}, [category, categoriesMap]);
 
 	return (
-		<div className="category-container">
-			{products &&
-				products.map((product) => (
-					<ProductCard key={product.id} product={product} />
-				))}
-		</div>
+		<Fragment>
+			<h2 className="category-title">{category.toUpperCase()}</h2>
+			<div className="category-container">
+				{products &&
+					products.map((product) => (
+						<ProductCard key={product.id} product={product} />
+					))}
+			</div>
+		</Fragment>
 	);
 };
 
