44c06bb74d73b13d7f568a3816a2b479fac4772a
unknown <miguel.semedo.2@outlook.com>
2022-10-24 10:44:18 +0100

    Cart Reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,5 +1,7 @@
 import { createContext, useEffect, useReducer } from 'react';
 
+import { createAction } from '../utils/reducer/reducer.utils';
+
 import {
 	onAuthStateChangedListener,
 	createUserDocumentFromAuth,
@@ -37,7 +39,7 @@ export const UserProvider = ({ children }) => {
 	const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
 
 	const setCurrentUser = (user) => {
-		dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+		dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
 	};
 
 	const value = { currentUser, setCurrentUser };
