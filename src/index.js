ffcf66993c0ec4d71c244f18036db180522fc0ff
unknown <miguel.semedo.2@outlook.com>
2022-10-31 12:51:12 +0000

    Setting up Stripe and Payment Form

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store, persistor } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js';
 
 import './index.scss';
 import App from './App';
+import { stripePromise } from './utils/stripe/stripe.utils';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,7 +16,9 @@ root.render(
 		<Provider store={store}>
 			<PersistGate loading={null} persistor={persistor}>
 				<BrowserRouter>
-					<App />
+					<Elements stripe={stripePromise}>
+						<App />
+					</Elements>
 				</BrowserRouter>
 			</PersistGate>
 		</Provider>
