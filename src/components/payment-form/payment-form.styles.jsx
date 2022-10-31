4a2e0a784ea4060acce23b17a0fbf905da28efaa
unknown <miguel.semedo.2@outlook.com>
2022-10-31 15:35:06 +0000

    Stripe Payment Functionality

--- src/components/payment-form/payment-form.styles.jsx
+++ src/components/payment-form/payment-form.styles.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { CardElement } from '@stripe/react-stripe-js';
 
 export const PaymentFormContainer = styled.div`
 	height: 150px;
@@ -11,4 +12,8 @@ export const PaymentFormContainer = styled.div`
 export const FormContainer = styled.form`
 	height: 100px;
 	min-width: 500px;
+`;
+
+export const CardInfo = styled(CardElement)`
+	margin-bottom: 20px;
 `;
